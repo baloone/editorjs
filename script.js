a989337522d5c7351da139c6fa8527e504390e5f
mohiceage <mohiceage@protonmail.com>
2020-11-12 22:02:36 +0100

    line numbers

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const editorDiv = document.querySelector('.editor');
+const linesDiv = document.querySelector('.lines');
 const oninput = function() {
     pre.innerHTML = hljs.highlightAuto(textarea.value).value;
+};
+
+const updatelines = function() {
+    linesDiv.innerHTML = textarea.value.split('\n').map((e,i) => `<span>${i+1}</span>`).join('<br>');
 };
 const updatecaret = function() {
     const offset = textarea.selectionDirection === "forward" ? textarea.selectionEnd : textarea.selectionStart;
     caretpre.innerHTML = pre.innerText.slice(0, offset)+'<span class="caret"></span>'+pre.innerText.slice(offset);
-}
+    updatelines();
+};
 textarea.addEventListener('input', oninput);
 textarea.addEventListener('input', updatecaret);
 textarea.addEventListener('click', updatecaret);
@@ -18,8 +24,5 @@ editorDiv.addEventListener('click', e => {
 window.addEventListener('click', e => {
     editorDiv.classList.remove("focused");
 });
-setTimeout(oninput)
-
-textarea.addEventListener('scroll', function() {
-    caretpre.scrollTop = pre.scrollTop = textarea.scrollTop;
-});
+setTimeout(oninput);
+setTimeout(updatecaret);
