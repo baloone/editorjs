054569f3dc65c77fd8b6e052b79313af36348067
mohiceage <mohiceage@protonmail.com>
2020-11-13 16:57:02 +0100

    add a timeout before highlighting

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+const editorContainerDiv = document.querySelector('.editor-container');
 const editorDiv = document.querySelector('.editor');
 const linesDiv = document.querySelector('.lines');
-const oninput = function() {
-    pre.innerHTML = hljs.highlightAuto(textarea.value).value;
+
+const highligh = function(timeout) {
+    var toId = null;
+    return function() {
+        if (toId) clearTimeout(toId);
+        toId = setTimeout(() => {
+            toId = null;
+            const res = hljs.highlightAuto(textarea.value);
+            editorContainerDiv.setAttribute('lang', res.language || "");
+            pre.innerHTML = res.value;
+        }, timeout);
+    }
+}(300);
+
+const oninput = function(e) {
+    pre.innerText = textarea.value;
+    highligh();
 };
 
 const updatelines = function() {
-    linesDiv.innerHTML = textarea.value.split('\n').map((e,i) => `<span>${i+1}</span>`).join('<br>');
+    const curLines = linesDiv.children.length;
+    const newLines = textarea.value.split('\n').length;
+    textarea.value.split('\n').slice(curLines).forEach((e,i) => {
+        const div = document.createElement('div');
+        div.innerText = curLines+i+1;
+        linesDiv.append(div);
+    });
+    [...linesDiv.children].slice(newLines).forEach(e => e.outerHTML = '');
 };
 const updatecaret = function() {
+    const span1 = document.createElement('span');
+    const spancaret = document.createElement('span');
+    const span2 = document.createElement('span');
     const offset = textarea.selectionDirection === "forward" ? textarea.selectionEnd : textarea.selectionStart;
-    caretpre.innerHTML = pre.innerText.slice(0, offset)+'<span class="caret"></span>'+pre.innerText.slice(offset);
+    span1.innerText = pre.innerText.slice(0, offset);
+    span2.innerText = pre.innerText.slice(offset);
+    spancaret.classList.add('caret');
+    caretpre.innerHTML = "";
+    caretpre.append(span1, spancaret, span2);
     updatelines();
 };
 textarea.addEventListener('input', oninput);
@@ -21,8 +51,7 @@ editorDiv.addEventListener('click', e => {
     editorDiv.classList.add("focused");
     e.stopPropagation();
 });
-window.addEventListener('click', e => {
-    editorDiv.classList.remove("focused");
-});
+window.addEventListener('click', () => editorDiv.classList.remove("focused"));
+window.addEventListener('blur', () => editorDiv.classList.remove("focused"));
 setTimeout(oninput);
 setTimeout(updatecaret);
